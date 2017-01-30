ae9d124004bcdff7f618f69c3e15b772c887af95
leichtjoon <ngk0106@naver.com>
2017-01-30 19:09:02 +0900

    dist !_!

--- dist/request.js
+++ dist/request.js
@@ -3,36 +3,39 @@
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 function optimizeCB(uri, method, opts, cb) {
+  //invalid uri
   if (typeof uri !== 'string') {
     throw new TypeError('uri must be String: ' + uri);
   }
-
+  // no options (cb only) --
   if (typeof cb === 'undefined') {
+    // invalid cb
     if (typeof opts !== 'function') {
       throw new TypeError('callback must be function: ' + opts);
     }
-    return [uri, { method: method }, opts];
-  } else if (typeof cb !== 'function') {
-    throw new TypeError('callback must be function: ' + cb);
+    return [uri, { method: method, data: {} }, opts];
   }
+  // have options, invalid cb
+  else if (typeof cb !== 'function') {
+      throw new TypeError('callback must be function: ' + cb);
+    }
 
+  // have options, invalid options
   if ((typeof opts === 'undefined' ? 'undefined' : _typeof(opts)) !== 'object') {
     throw new TypeError('options muse be object{}: ' + opts);
   }
 
-  return [uri, opts, cb];
+  return [uri, Object.assign({
+    data: {}
+  }, opts, {
+    method: method
+  }), cb];
 }
 
 function request(uri, opts, cb) {
   var httpRequest = this.httpRequest;
   var isOptions = typeof cb === 'undefined';
   var options = isOptions ? {} : opts;
-
-  if (isOptions) {
-    cb = opts;
-    options.method = 'GET';
-    options.data = {};
-  }
 
   if (typeof module !== 'undefined' && module.exports) {
     // CommonJS
@@ -59,6 +62,10 @@ function request(uri, opts, cb) {
   }
 }
 
+function all(uri, opts, cb) {
+  request.apply(this, optimizeCB(uri, opts.method || 'GET', opts, cb));
+}
+
 function get(uri, opts, cb) {
   request.apply(this, optimizeCB(uri, 'GET', opts, cb));
 }
@@ -75,7 +82,7 @@ function del(uri, opts, cb) {
   request.apply(this, optimizeCB(uri, 'DELETE', opts, cb));
 }
 
-module.exports.request = request;
+module.exports.request = all;
 module.exports.get = get;
 module.exports.post = post;
 module.exports.put = put;
